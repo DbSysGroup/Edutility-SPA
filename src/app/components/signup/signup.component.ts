2bb532a7d9f201f444149de797455f530d147d03
Brandon Grant <grant_b@utpb.edu>
2018-10-31 12:26:09 -0500

    Updated sign up form

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogConfig, MatDialogRef } from '@angular/material';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+
+const email_regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 @Component({
   selector: 'app-signup',
@@ -8,17 +12,58 @@ import { MatDialog, MatDialogConfig, MatDialogRef } from '@angular/material';
 })
 export class SignupComponent implements OnInit {
 
-  constructor(private dialog: MatDialog) { }
+  constructor(private dialog: MatDialog, private auth: AuthService) { }
+
+  signupForm: FormGroup
 
   ngOnInit() {
+    this.signupForm = new FormGroup({
+      username: new FormControl('', [
+        Validators.required
+      ]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern(email_regex)
+      ]),
+      password: new FormControl('', [
+        Validators.required
+      ]),
+      school: new FormControl(''),
+      dob: new FormControl('')
+    });
   }
 
-  openDialog() {
+  get username(): string {
+    return this.signupForm.get('username').value;
+  }
+  get email(): string {
+    return this.signupForm.get('email').value;
+  }
+  get password(): string {
+    return this.signupForm.get('password').value;
+  }
+  get school(): string {
+    return this.signupForm.get('school').value;
+  }
+  get dob(): Date {
+    return this.signupForm.get('dob').value;
+  }
+
+  logIn() {
+    const creds = {
+      email: this.email,
+      password: this.password
+    }
+    console.log(creds.email, creds.password);
+    this.auth.signUp(creds);
+  }
+
+  openDialog() { 
     const dialogConfig = new MatDialogConfig();
     dialogConfig.disableClose = true;
     dialogConfig.autoFocus = true;
-    dialogConfig.width = "50%";
-    dialogConfig.height = "60%";
+    dialogConfig.width = "450px";
+    dialogConfig.height = "600px";
     this.dialog.open(SignupComponent, dialogConfig);
   }
 
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -25,4 +25,8 @@ export class AuthService {
       })
     );
   }
+
+  signUp(credentials) {
+
+  }
 }
